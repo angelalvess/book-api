db3cdd3e969678632cf5db5949ab861d458a1904
angelalves <angeltrashangel@gmail.com>
2024-09-19 03:27:06 -0300

    Add permission class to BooksViewSet

--- books/api/viewsets.py
+++ books/api/viewsets.py
@@ -1,8 +1,11 @@
 from rest_framework import viewsets
+from rest_framework import permissions
 from books.api import serializers
 from books.models import Books
 
 
 class BooksViewSet(viewsets.ModelViewSet):
+
+    permission_classes = [permissions.IsAuthenticated]
     serializer_class = serializers.BooksSerializer
     queryset = Books.objects.all()
--- setup/urls.py
+++ setup/urls.py
@@ -6,6 +6,7 @@ from rest_framework import routers
 from books.api.viewsets import BooksViewSet
 from django.conf import settings
 from django.conf.urls.static import static
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 route = routers.DefaultRouter()
 
@@ -14,4 +15,6 @@ route.register(r'books', BooksViewSet, basename='Books')
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(route.urls)),
+    path('token/', TokenObtainPairView.as_view()),
+    path('token/refresh/', TokenRefreshView.as_view()),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
